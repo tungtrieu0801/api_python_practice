70f4af837becab8e1cafe28ad96a113cfedf0949
tungtrieu0801 <trieutungvp@gmail.com>
2024-02-16 10:46:48 +0700

    first commit

---
+++ drinks/drinks/serializers.py
@@ -0,0 +1,8 @@
+#transfer from models to json
+
+from rest_framework import serializers
+from .models import Drink
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
